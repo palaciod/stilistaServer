4e3f5648665ca3e48e987a40edb28b28065b1bee
Daniel Palacio <dpalacio@buffalo.edu>
2020-01-27 19:47:53 -0500

    Forgot Mongodb atlas url

--- config/Database.js
+++ config/Database.js
@@ -1,5 +1,8 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = { mongoURI: "LOL NOT POSTING THIS ON GITHUB" };
+  module.exports = {
+    mongoURI:
+      "mongodb+srv://palaciod:Dan8/!^/96@stilista-fkgeb.mongodb.net/test?retryWrites=true&w=majority"
+  };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/stilista-dev" };
 }
